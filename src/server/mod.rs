03bcee6a29e02a9f2fade58e485382a0dc60f9e3
paulobressan <paulo.bressan@outlook.com>
2025-01-22 14:54:25 -0300

    feat: grpc server with u5c submit service

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,7 +1,33 @@
 use anyhow::Result;
+use pallas::interop::utxorpc::spec as u5c;
+use std::{net::SocketAddr, str::FromStr};
+use tonic::transport::Server;
+use tracing::info;
 
-pub mod utxorpc;
+mod utxorpc;
 
 pub async fn run() -> Result<()> {
+    let submit_service = utxorpc::SubmitServiceImpl {};
+    let submit_service =
+        u5c::submit::submit_service_server::SubmitServiceServer::new(submit_service);
+
+    let reflection = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(u5c::submit::FILE_DESCRIPTOR_SET)
+        .register_encoded_file_descriptor_set(protoc_wkt::google::protobuf::FILE_DESCRIPTOR_SET)
+        .build_v1()
+        .unwrap();
+
+    let mut server = Server::builder().accept_http1(true);
+
+    let address = "0.0.0.0:5000";
+    info!(address, "GRPC server running");
+
+    let address = SocketAddr::from_str(address)?;
+    server
+        .add_service(reflection)
+        .add_service(submit_service)
+        .serve(address)
+        .await?;
+
     Ok(())
 }
---
+++ src/server/utxorpc.rs
@@ -0,0 +1,50 @@
+use futures_core::Stream;
+use pallas::interop::utxorpc::spec::submit::{WaitForTxResponse, *};
+use std::pin::Pin;
+use tonic::{Request, Response, Status};
+
+pub struct SubmitServiceImpl {}
+
+#[async_trait::async_trait]
+impl submit_service_server::SubmitService for SubmitServiceImpl {
+    type WaitForTxStream =
+        Pin<Box<dyn Stream<Item = Result<WaitForTxResponse, tonic::Status>> + Send + 'static>>;
+
+    type WatchMempoolStream =
+        Pin<Box<dyn Stream<Item = Result<WatchMempoolResponse, tonic::Status>> + Send + 'static>>;
+
+    async fn submit_tx(
+        &self,
+        _request: Request<SubmitTxRequest>,
+    ) -> Result<Response<SubmitTxResponse>, Status> {
+        todo!()
+    }
+
+    async fn wait_for_tx(
+        &self,
+        _request: Request<WaitForTxRequest>,
+    ) -> Result<Response<Self::WaitForTxStream>, Status> {
+        todo!()
+    }
+
+    async fn read_mempool(
+        &self,
+        _request: tonic::Request<ReadMempoolRequest>,
+    ) -> Result<tonic::Response<ReadMempoolResponse>, tonic::Status> {
+        Err(Status::unimplemented("read_mempool is not yet available"))
+    }
+
+    async fn watch_mempool(
+        &self,
+        _request: tonic::Request<WatchMempoolRequest>,
+    ) -> Result<tonic::Response<Self::WatchMempoolStream>, tonic::Status> {
+        todo!()
+    }
+
+    async fn eval_tx(
+        &self,
+        _request: tonic::Request<EvalTxRequest>,
+    ) -> Result<tonic::Response<EvalTxResponse>, Status> {
+        todo!()
+    }
+}
