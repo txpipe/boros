bea0d49951418b9a3d951b56598d09f443db5cd9
paulobressan <paulo.bressan@outlook.com>
2025-01-23 10:04:26 -0300

    chore: whapped grpc server into a tokio thread

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -2,32 +2,44 @@ use anyhow::Result;
 use pallas::interop::utxorpc::spec as u5c;
 use std::{net::SocketAddr, str::FromStr};
 use tonic::transport::Server;
-use tracing::info;
+use tracing::{error, info};
 
 mod utxorpc;
 
 pub async fn run() -> Result<()> {
-    let submit_service = utxorpc::SubmitServiceImpl {};
-    let submit_service =
-        u5c::submit::submit_service_server::SubmitServiceServer::new(submit_service);
+    tokio::spawn(async {
+        let submit_service = utxorpc::SubmitServiceImpl {};
+        let submit_service =
+            u5c::submit::submit_service_server::SubmitServiceServer::new(submit_service);
 
-    let reflection = tonic_reflection::server::Builder::configure()
-        .register_encoded_file_descriptor_set(u5c::submit::FILE_DESCRIPTOR_SET)
-        .register_encoded_file_descriptor_set(protoc_wkt::google::protobuf::FILE_DESCRIPTOR_SET)
-        .build_v1()
-        .unwrap();
+        let reflection = tonic_reflection::server::Builder::configure()
+            .register_encoded_file_descriptor_set(u5c::submit::FILE_DESCRIPTOR_SET)
+            .register_encoded_file_descriptor_set(protoc_wkt::google::protobuf::FILE_DESCRIPTOR_SET)
+            .build_v1()
+            .unwrap();
 
-    let mut server = Server::builder().accept_http1(true);
+        let mut server = Server::builder().accept_http1(true);
 
-    let address = "0.0.0.0:5000";
-    info!(address, "GRPC server running");
+        let address = "0.0.0.0:5000";
+        info!(address, "GRPC server running");
 
-    let address = SocketAddr::from_str(address)?;
-    server
-        .add_service(reflection)
-        .add_service(submit_service)
-        .serve(address)
-        .await?;
+        let result = SocketAddr::from_str(address);
+        if let Err(error) = result {
+            error!(?error);
+            std::process::exit(1);
+        }
+
+        let result = server
+            .add_service(reflection)
+            .add_service(submit_service)
+            .serve(result.unwrap())
+            .await;
+
+        if let Err(error) = result {
+            error!(?error);
+            std::process::exit(1);
+        }
+    });
 
     Ok(())
 }
