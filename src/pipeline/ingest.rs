69847b535bb399b4af938311126992b2d353373e
Clark Alesna <clark_alesna@hotmail.com>
2025-02-27 19:39:38 +0800

    refactor: update sorting algorithm (#31)
    * Implement sorting algorithm (#25)
    
    * feat: updated priority to queue
    
    * feat: implemented priority config
    
    * feat: added priority module
    
    * chore: adjusted lint
    
    * feat: implemented priority quote
    
    * feat: implemented storage query to be used in priority module
    
    * test: implemented priority test
    
    * chore: removed dbg
    
    * chore: added validation when the state is empty
    
    * docs: added priority docs
    
    * chore: updated config structure
    
    * docs: updated docs
    
    * chore: changed workflow job name
    
    * chore: changed workflow job name
    
    * chore: updated quota to cap
    
    * docs: updated priority docs
    
    ---------
    
    Co-authored-by: Paulo Bressan <paulo.bressan@outlook.com>

--- src/pipeline/ingest.rs
+++ src/pipeline/ingest.rs
@@ -8,6 +8,7 @@ use crate::{
     priority::Priority,
     storage::{sqlite::SqliteTransaction, Transaction, TransactionStatus},
 };
+use super::CAP;
 
 #[derive(Stage)]
 #[stage(name = "ingest", unit = "Vec<Transaction>", worker = "Worker")]
@@ -36,7 +37,7 @@ impl gasket::framework::Worker<Stage> for Worker {
     ) -> Result<WorkSchedule<Vec<Transaction>>, WorkerError> {
         let transactions = stage
             .priority
-            .next(TransactionStatus::Pending)
+            .next(TransactionStatus::Pending, CAP)
             .await
             .or_retry()?;
 
